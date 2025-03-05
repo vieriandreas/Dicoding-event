495b391cb99c26d3a28b9031ab9267f3cc6989e1
vieriandreas <vierinapitupulu7@gmail.com>
2025-03-05 20:33:12 +0700

    edit EventAdapter

--- app/src/main/java/com/example/dicodingevent/EventAdapter.kt
+++ app/src/main/java/com/example/dicodingevent/EventAdapter.kt
@@ -3,35 +3,42 @@ package com.example.dicodingevent
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dicodingevent.data.response.EventsItem
 import com.example.dicodingevent.databinding.EventItemBinding
 import com.example.dicodingevent.ui.MainActivity
 
-class EventAdapter : RecyclerView.Adapter<EventItemViewHolder>() {
-
-    private val list = mutableListOf<EventsItem>()
+class EventAdapter : ListAdapter<EventsItem,EventItemViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventItemViewHolder {
         val binding = EventItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return EventItemViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int = currentList.size
 
     override fun onBindViewHolder(holder: EventItemViewHolder, position: Int) {
-        holder.bind(list[position])
+        holder.bind(currentList[position])
 
         holder.itemView.setOnClickListener {
             val intent = Intent(holder.itemView.context, MainActivity::class.java)
-            intent.putExtra("key_event", list[holder.adapterPosition])
+            intent.putExtra("key_event", currentList[holder.adapterPosition])
             holder.itemView.context.startActivity(intent)
         }
     }
 
-    fun submitList(listEventsItem: List<EventsItem>) {
-        list.addAll(listEventsItem)
-        notifyDataSetChanged()
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<EventsItem>() {
+            override fun areItemsTheSame(oldItem: EventsItem, newItem: EventsItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: EventsItem, newItem: EventsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 
 }
